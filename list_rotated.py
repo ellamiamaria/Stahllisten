94d1ef29ddce76483c77f93e15aa32ca9ec8ffba
ellamiamaria <ella.gekeler@gmx.de>
2021-11-08 19:04:01 +0100

    final

--- list_rotated.py
+++ list_rotated.py
@@ -1,15 +1,12 @@
 import pandas
 import tabula
 import pandas as pd
-import numpy as np
 from extract_files import extract_all_files
-import io
 
 
 def get_clean_list_from_pdf(liste):
-    index = ["6", "8", "10", "12", "14", "16", "18", "20", "22", "25", "26", "28", "32", "36", "40"]
-    plans = pandas.DataFrame(index=index)
-    x = 1
+    plans = pandas.DataFrame()
+    plans["DN mm"] = ["6", "8", "10", "12", "14", "16", "18", "20", "22", "25", "26", "28", "32", "36", "40"]
     for files in liste:
         """read PDF with Tabula& convert into *.csv File"""
         tabula.convert_into(input_path=fr"E:\Projekte\Stahllisten\Listen\{files}",
@@ -18,19 +15,10 @@ def get_clean_list_from_pdf(liste):
 
         """Import *.csv file as Pandas Dataframe"""
         new_data = pd.read_csv(r"E:\Projekte\Stahllisten\Output\outputliste.csv", header=None)
-        print(new_data)
         new_data = new_data.fillna(0)
-        print(new_data)
-        plans[f"fix Gew.kg{x}"] = new_data[2]
-        print(plans)
-        plans[f"bearb. Gew. kg{x}"] = new_data[6]
-        plans[f"{files}"] = plans[f"fix Gew.kg{x}"] + plans[f"bearb. Gew. kg{x}"]
-        x += 1
+        plans[f"{files}"] = new_data[2] + new_data[6]
 
-    print(plans)
-
-
-
+    plans.to_excel(r"E:\Projekte\Stahllisten\Output\Stahllisteconv.xlsx")
 
 
 get_clean_list_from_pdf(extract_all_files())
